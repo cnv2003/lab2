2dfd8bfd361d60ec96f116fe82fe56e74342715c
cnv2003 <chalanika03@gmail.com>
2024-01-31 13:57:05 +0530

    Add project files.

---
+++ lab2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the length:");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.Write("enter the width:");
+            int width = int.Parse(Console.ReadLine());
+
+            int area = length * width;
+
+            Console.WriteLine("the area of the rectangle is:\n" + area);
+            Console.ReadLine();
+                
+
+
+        }
+    }
+}
